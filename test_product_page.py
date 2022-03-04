625fd7f9e19222548665551526e7817890652ef7
AleksandrIvanov2021 <92321006+AleksandrIvanov2021@users.noreply.github.com>
2022-03-04 15:43:33 +0300

    add tests_success_message

--- test_product_page.py
+++ test_product_page.py
@@ -29,7 +29,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
-    product_page.solve_quiz_and_get_code()
     product_page.should_not_be_success_message()
 
 
