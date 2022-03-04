47075cb60a91de73942352140db70422185ca510
AleksandrIvanov2021 <92321006+AleksandrIvanov2021@users.noreply.github.com>
2022-03-04 15:32:29 +0300

    add tests_success_message

--- test_product_page.py
+++ test_product_page.py
@@ -23,6 +23,16 @@ def test_guest_can_add_product_to_basket(browser, number):
     product_page.should_be_book_price()
 
 
+@pytest.mark.xfail(reason="test should fall because guest can see success message after adding product to basket")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_not_be_success_message()
+
+
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
