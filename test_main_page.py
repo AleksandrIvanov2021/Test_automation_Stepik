847f554b9700547ba477defecf0df40bc03b8e1e
AleksandrIvanov2021 <92321006+AleksandrIvanov2021@users.noreply.github.com>
2022-03-04 11:45:09 +0300

    add test_guest_should_see_login_page

---
+++ test_main_page.py
@@ -0,0 +1,28 @@
+from .PageObject.main_page import MainPage
+from .PageObject.login_page import LoginPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page2 = LoginPage(browser, link)
+    page2.open()
+    page2.should_be_login_page()
+
+    # pytest -v --tb=line --language=en 1_test_main_page.py
+
