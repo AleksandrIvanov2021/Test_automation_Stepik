0ce01368cc181226660115953253ebd3994d9f4a
AleksandrIvanov2021 <92321006+AleksandrIvanov2021@users.noreply.github.com>
2022-03-04 14:21:37 +0300

    add test_guest_cant_see_success_message

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,15 @@ from .PageObject.product_page import ProductPage
 import pytest
 
 
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_be_book_name()
+    product_page.should_be_book_price()
+
+
 @pytest.mark.parametrize('number', [0, 1, 2, 3, 4, 5, 6,
                                     pytest.param(7, marks=pytest.mark.xfail(reason="fixing this bug right now")), 8, 9])
 def test_guest_can_add_product_to_basket(browser, number):
@@ -12,5 +21,21 @@ def test_guest_can_add_product_to_basket(browser, number):
     product_page.solve_quiz_and_get_code()
     product_page.should_be_book_name()
     product_page.should_be_book_price()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="not disappiring")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.is_dissappeared()
 
     # pytest -rx -v test_product_page.py
