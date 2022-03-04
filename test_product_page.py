6b7a998223222772ca67f96c7a2eb74f351e4a25
AleksandrIvanov2021 <92321006+AleksandrIvanov2021@users.noreply.github.com>
2022-03-04 12:48:49 +0300

    add mark.parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,11 @@
 from .PageObject.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('number', [0, 1, 2, 3, 4, 5, 6,
+                                    pytest.param(7, marks=pytest.mark.xfail(reason="fixing this bug right now")), 8, 9])
+def test_guest_can_add_product_to_basket(browser, number):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{number}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
@@ -10,3 +13,4 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.should_be_book_name()
     product_page.should_be_book_price()
 
+    # pytest -rx -v test_product_page.py
